8b49f696eb82cfdf28c1a05fd9383a61f6d26f10
Don Dyu <ddkim@connect.ust.hk>
2017-04-18 17:07:28 -0700

    minor edits

--- models/connect.js
+++ models/connect.js
@@ -1,1 +1,2 @@
-module.exports = "mongodb://<username>:<password>@ds139939.mlab.com:39939/don-horizons" //TODO: Enter your MongoDb credentials or pass it through Environment Variable
+module.exports = "mongodb://<username>:<password>@ds139939.mlab.com:39939/don-horizons"
+//TODO: Enter your MongoDb credentials or pass it through Environment Variable
